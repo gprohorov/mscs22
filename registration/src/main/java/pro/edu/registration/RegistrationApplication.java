71e0ae0bf1f5d542c42c19ed5dcad2bf9b7e57c3
gprohorov <george.prokhorov@external.getneteurope.com>
2022-10-19 23:22:46 +0300

    Initial commit

---
+++ registration/src/main/java/pro/edu/registration/RegistrationApplication.java
@@ -0,0 +1,18 @@
+package pro.edu.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+  @author   george
+  @project   mscs22
+  @class  RegistrationApplication
+  @version  1.0.0 
+  @since 19.10.22 - 23.10
+*/
+@SpringBootApplication
+public class RegistrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationApplication.class,args);
+    }
+}
