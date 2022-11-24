ab3a359e96744fad414a8afa15504594c6cc0976
gprohorov <george.prokhorov@external.getneteurope.com>
2022-11-24 12:26:52 +0200

    cheat module

---
+++ cheat/src/main/java/pro/edu/cheat/CheatApplication.java
@@ -0,0 +1,18 @@
+package pro.edu.cheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+  @author   george
+  @project   mscs22
+  @class  CheatApplication
+  @version  1.0.0 
+  @since 24.11.22 - 11.52
+*/
+@SpringBootApplication
+public class CheatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CheatApplication.class, args);
+    }
+}
